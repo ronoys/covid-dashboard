1ab364ad09c370bb978c050a8c119bb095c0eaf0
ronoy sarkar <38772534+ronoys@users.noreply.github.com>
2021-02-05 09:52:27 -0500

    v2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import { makeStyles , ThemeProvider} from "@material-ui/core/styles";
+import theme from './theme'
+import 'typeface-roboto'
+import palette from './theme/palette'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload. Test. 
+          Covid-19 Dashboard 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="outlined" color="secondary">Test Button</Button>
       </header>
     </div>
   );
