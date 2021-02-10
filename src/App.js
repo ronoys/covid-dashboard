008b0578c6d5145edf1b15c08eba72dcce499c5f
ronoy sarkar <38772534+ronoys@users.noreply.github.com>
2021-02-09 21:05:58 -0500

    app layout changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import './App.css';
-import Button from '@material-ui/core/Button';
-import { makeStyles , ThemeProvider} from "@material-ui/core/styles";
-import theme from './theme'
-import 'typeface-roboto'
-import palette from './theme/palette'
-
+import './Tabs'
+import FullWidthTabs from './Tabs';
+import Card from './card'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Covid-19 Dashboard 
-        </p>
-        <Button variant="outlined" color="secondary">Test Button</Button>
-      </header>
+      <div className="App-header">
+      <FullWidthTabs></FullWidthTabs>
+      
+    </div>
+
+      <Card className="Card"></Card>
     </div>
   );
 }
