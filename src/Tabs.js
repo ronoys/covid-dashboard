4388bca371604416dd6afc2ee4b68b53ae1bef9a
ronoy sarkar <38772534+ronoys@users.noreply.github.com>
2021-02-19 09:48:31 -0500

    v3

--- src/Tabs.js
+++ src/Tabs.js
@@ -7,8 +7,8 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import CenteredGrid from './Grid'
-
+import CenteredGrid from './updatesGrid'
+import Sales from './chart'
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -90,7 +90,7 @@ export default function FullWidthTabs() {
         
         </TabPanel>
         <TabPanel value={value} index={1} dir={theme.direction}>
-          Content #2
+          <Sales></Sales>
           
         </TabPanel>
         <TabPanel value={value} index={2} dir={theme.direction}>
