b7c40c51220bf3e8f15c79c5a17838e8b121844c
ronoy sarkar <38772534+ronoys@users.noreply.github.com>
2021-03-24 21:38:01 -0400

    Using Hooks for API Fetch (working)

--- src/mainViews/casesAPI.js
+++ src/mainViews/casesAPI.js
@@ -1,51 +1,57 @@
 import React, {useEffect, useState} from 'react'
 
+function MyComponent() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-export default function MyComponent() {
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all?yesterday=true&twoDaysAgo=true&allowNull=true")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
 
-    return(
-        <div>
-        <p>Hello</p>
-        </div>
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+
+        
+        
+          <p>
+            {items.updated} {items.cases}
+          </p>
+        
+      </ul>
     );
-
-    /* const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
-  
-    
-    useEffect(() => {
-      fetch("https://api.example.com/items")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setIsLoaded(true);
-            setItems(result);
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    }, [])
-  
-    
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <ul>
-          {items.map(item => (
-            <li key={item.id}>
-              {item.name} {item.price}
-            </li>
-          ))}
-        </ul>
-      );
-    } */
   }
+}
+
+export default function Test123() {
+  
+  return (
+    <div>
+      <MyComponent></MyComponent>
+    </div>
+  );
+}
+
+    
